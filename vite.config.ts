f96cc98a4b4245824b6d76aa61adce4a8fb32dde
Joey Storto <joe.storto@gmail.com>
2026-06-10 16:43:39 -0400

    github flows

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig  } from 'vite';
 
 // Vite config
 export default defineConfig({
+    base: '/iso-maze',
     server: {
         port: 5173,
         open: true,
-    },
+    }
 });
